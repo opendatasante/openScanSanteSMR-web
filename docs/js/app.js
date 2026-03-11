a3bb9001ab4bcf07b56764e5233a85b09717dad8
sebastiencys <263411793+sebastiencys@users.noreply.github.com>
2026-03-11 12:31:02 +0100

    Update app.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -81,8 +81,6 @@ async function init() {
             pageLength: 15,
             dom: '<"top"f>rt<"bottom"lip><"clear">'
         });
-
-        initMap();
         
         table.on('draw', function () {
             updateGlobalStats();
@@ -130,6 +128,7 @@ async function init() {
         document.getElementById("main-table_wrapper").style.display = "none";
         document.getElementById("map-view").style.display = "block";
 
+        initMap();
         refreshViews();
     };
 
